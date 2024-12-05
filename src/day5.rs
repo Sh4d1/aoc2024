959360821fab368308921cab4f4bf30d05460d5e
Patrik Cyvoct <patrik@ptrk.io>
2024-12-05 06:31:37 +0100

    day5
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

---
+++ src/day5.rs
@@ -0,0 +1,115 @@
+use itertools::Itertools;
+
+#[derive(Debug, Clone)]
+pub struct Input {
+    rules: Vec<(usize, usize)>,
+    updates: Vec<Vec<usize>>,
+}
+
+#[aoc_generator(day5)]
+pub fn parse(input: &str) -> Input {
+    let (one, two) = input.split_once("\n\n").unwrap();
+    Input {
+        rules: one
+            .lines()
+            .map(|l| l.split_once('|').unwrap())
+            .map(|(a, b)| (a.parse().unwrap(), b.parse().unwrap()))
+            .collect(),
+        updates: two
+            .lines()
+            .map(|l| l.split(",").map(|s| s.parse::<usize>().unwrap()).collect())
+            .collect(),
+    }
+}
+
+#[aoc(day5, part1)]
+pub fn part1(input: &Input) -> usize {
+    input
+        .updates
+        .iter()
+        .filter(|u| {
+            u.iter().tuple_windows().all(|(&a, &b)| {
+                for r in &input.rules {
+                    if a == r.0 && b == r.1 {
+                        return true;
+                    }
+                }
+                false
+            })
+        })
+        .fold(0, |acc, u| acc + *u.get(u.len() / 2).unwrap())
+}
+
+#[aoc(day5, part2)]
+pub fn part2(input: &Input) -> usize {
+    input
+        .updates
+        .iter()
+        .filter(|u| {
+            !u.iter().tuple_windows().all(|(&a, &b)| {
+                for r in &input.rules {
+                    if a == r.0 && b == r.1 {
+                        return true;
+                    }
+                }
+                false
+            })
+        })
+        .fold(0, |acc, u| {
+            acc + u
+                .iter()
+                .sorted_by(|&a, &b| {
+                    for r in &input.rules {
+                        if *a == r.0 && *b == r.1 {
+                            return std::cmp::Ordering::Less;
+                        } else if *a == r.1 && *b == r.0 {
+                            return std::cmp::Ordering::Greater;
+                        }
+                    }
+                    unreachable!()
+                })
+                .nth(u.len() / 2)
+                .unwrap()
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+    #[test]
+    fn example1() {
+        assert_eq!(part1(&parse(INPUT)), 143)
+    }
+    #[test]
+    fn example2() {
+        assert_eq!(part2(&parse(INPUT)), 123)
+    }
+}

ec2312326549f09bad7c25037a8762c50af78222
Patrik Cyvoct <patrik@ptrk.io>
2024-12-06 10:48:53 +0100

    day6: bitflags to a faster solution (p2 ~90ms)
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

--- src/day6.rs
+++ src/day6.rs
@@ -1,4 +1,27 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
+
+use bitflags::bitflags;
+
+bitflags! {
+   #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+   struct GuardDirections: u8 {
+       const UP = 1;
+       const DOWN = 1 << 1;
+       const LEFT = 1 << 2;
+       const RIGHT = 1 << 3;
+   }
+}
+
+impl From<Direction> for GuardDirections {
+    fn from(direction: Direction) -> Self {
+        match direction {
+            Direction::Up => GuardDirections::UP,
+            Direction::Down => GuardDirections::DOWN,
+            Direction::Left => GuardDirections::LEFT,
+            Direction::Right => GuardDirections::RIGHT,
+        }
+    }
+}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub enum Direction {
@@ -25,15 +48,6 @@ impl Direction {
             Direction::Down => Direction::Left,
             Direction::Left => Direction::Up,
         }
-    }
-
-    pub fn all() -> [Self; 4] {
-        [
-            Direction::Up,
-            Direction::Right,
-            Direction::Down,
-            Direction::Left,
-        ]
     }
 }
 
@@ -89,11 +103,12 @@ pub fn parse(input: &str) -> Input {
 
 #[aoc(day6, part1)]
 pub fn part1(input: &Input) -> usize {
-    let mut visited = HashSet::new();
-    let mut visited_no_dir = HashSet::new();
+    let mut visited = vec![vec![GuardDirections::empty(); input.w]; input.h];
+
     let mut guard = input.guard_start;
-    visited.insert(guard);
-    visited_no_dir.insert((guard.0, guard.1));
+    visited[guard.0][guard.1] = guard.2.into();
+    let mut res = 1;
+
     loop {
         let next_pos = (
             guard.0.wrapping_add_signed(guard.2.get_delta().0),
@@ -102,49 +117,51 @@ pub fn part1(input: &Input) -> usize {
         if next_pos.0 >= input.h || next_pos.1 >= input.w {
             break;
         }
+
         if let Cell::Used = input.grid[next_pos.0][next_pos.1] {
             guard = (guard.0, guard.1, guard.2.rotate());
         } else {
             guard = (next_pos.0, next_pos.1, guard.2);
         }
-        if visited.contains(&guard) {
+        if visited[guard.0][guard.1].contains(guard.2.into()) {
             break;
         }
-        visited.insert(guard);
-        visited_no_dir.insert((guard.0, guard.1));
+
+        if visited[guard.0][guard.1].is_empty() {
+            res += 1;
+        }
+        visited[guard.0][guard.1].insert(guard.2.into());
     }
-    visited_no_dir.len()
+    res
 }
 
 #[aoc(day6, part2)]
 pub fn part2(input: &Input) -> usize {
-    let mut visited = HashSet::new();
+    let mut visited = vec![vec![GuardDirections::empty(); input.w]; input.h];
+
     let mut guard = input.guard_start;
+    visited[guard.0][guard.1] = guard.2.into();
     let mut res = HashSet::new();
-    visited.insert(guard);
 
     loop {
-        let (dx, dy) = guard.2.get_delta();
         let next_pos = (
-            guard.0.wrapping_add_signed(dx),
-            guard.1.wrapping_add_signed(dy),
+            guard.0.wrapping_add_signed(guard.2.get_delta().0),
+            guard.1.wrapping_add_signed(guard.2.get_delta().1),
         );
         if next_pos.0 >= input.h || next_pos.1 >= input.w {
             break;
         }
+
         if let Cell::Used = input.grid[next_pos.0][next_pos.1] {
             guard = (guard.0, guard.1, guard.2.rotate());
         } else {
             let new_block = next_pos;
 
-            if !Direction::all()
-                .iter()
-                .any(|d| visited.contains(&(new_block.0, new_block.1, *d)))
-            {
+            if visited[new_block.0][new_block.1].is_empty() {
                 let mut new_guard = (guard.0, guard.1, guard.2.rotate());
                 let mut new_visited = visited.clone();
-                new_visited.insert(new_guard);
-                new_visited.insert(guard);
+                new_visited[new_guard.0][new_guard.1] = new_guard.2.into();
+                new_visited[guard.0][guard.1] = guard.2.into();
                 loop {
                     let new_guard_pos = (
                         new_guard.0.wrapping_add_signed(new_guard.2.get_delta().0),
@@ -160,20 +177,21 @@ pub fn part2(input: &Input) -> usize {
                     } else {
                         new_guard = (new_guard_pos.0, new_guard_pos.1, new_guard.2);
                     }
-                    if new_visited.contains(&new_guard) {
+                    if new_visited[new_guard.0][new_guard.1].contains(new_guard.2.into()) {
                         res.insert(new_block);
                         break;
                     }
-                    new_visited.insert(new_guard);
+                    new_visited[new_guard.0][new_guard.1].insert(new_guard.2.into());
                 }
             }
 
             guard = (next_pos.0, next_pos.1, guard.2);
         }
-        if visited.contains(&guard) {
+
+        if visited[guard.0][guard.1].contains(guard.2.into()) {
             break;
         }
-        visited.insert(guard);
+        visited[guard.0][guard.1] = guard.2.into();
     }
     res.len()
 }
